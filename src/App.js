2430d36ce08b5231a54d421e06f9c1ba5bb0e8e3
ylose <heungeob1003@gmail.com>
2024-02-05 13:57:41 +0900

    Todo로 item 넘겨주기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
+import React, { useState } from "react";
 
 function App() {
+
+  const [item, setItem] = useState({
+    id: "0",
+    title: "Hello World 1",
+    done: true }
+    );
+
   return (
     <div className="App">
-      <Todo />
-      <Todo />
+      <Todo item={item}/>
     </div>
   );
 }
