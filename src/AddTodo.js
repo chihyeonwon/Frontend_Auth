224b4101107ea2f1c5ef1a3a8f7aa1588fcac002
ylose <heungeob1003@gmail.com>
2024-02-06 12:34:13 +0900

    onInputChange 함수 작성

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -5,10 +5,21 @@ const AddTodo = props => {
   // 사용자의 입력을 지정할 컴포넌트
   const [item, setItem] = useState({ title: "" });
 
+  // onInputChange 함수 작성
+  const onInputChange = e => {
+    setItem({ title: e.target.value });
+    console.log(item);
+  };
+
   return (
     <Grid container style={{ marginTop: 20 }}>
       <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-        <TextField placeholde='Add Todo here' fullWidth />
+        <TextField
+          placeholde='Add Todo here'
+          fullWidth
+          onChange={onInputChange}
+          value={item.title}
+        />
       </Grid>
       <Grid xs={1} md={1} item>
         <Button
