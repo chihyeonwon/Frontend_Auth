88a41d59a093cce4b8171ca9b063113b36c434b4
ylose <heungeob1003@gmail.com>
2024-02-23 01:45:32 +0900

    네비게이션 바와 로그아웃 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
 import Todo from "./Todo";
 import React, { useEffect, useState } from "react";
-import { Container, List, Paper } from "@mui/material";
+import {
+  Container,
+  List,
+  Paper,
+  Grid,
+  Button,
+  AppBar,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import AddTodo from "./AddTodo";
-import { call } from "./service/ApiService";
+import { call, signout } from "./service/ApiService";
 
 function App() {
   const [items, setItems] = useState([]);
@@ -39,8 +48,27 @@ function App() {
     </Paper>
   );
 
+  // navigationBar 추가
+  let navigationBar = (
+    <AppBar position='static'>
+      <Toolbar>
+        <Grid justifyContent='space-between' container>
+          <Grid item>
+            <Typography variant='h6'>오늘의 할일</Typography>
+          </Grid>
+          <Grid item>
+            <Button color='inherit' raised onClick={signout}>
+              로그아웃
+            </Button>
+          </Grid>
+        </Grid>
+      </Toolbar>
+    </AppBar>
+  );
+
   return (
     <div className='App'>
+      {navigationBar} {/* 네비게이션 바 렌더링 */}
       <Container maxWidth='md'>
         <AddTodo addItem={addItem} />
         <div className='TodoList'>{todoItems}</div>
