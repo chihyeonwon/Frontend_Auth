4b41d6db71216d5876223256ad729930182c3fd9
ylose <heungeob1003@gmail.com>
2024-02-23 01:57:55 +0900

    로딩 중 로직 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Todo from "./Todo";
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Container,
   List,
@@ -16,21 +16,25 @@ import { call, signout } from "./service/ApiService";
 
 function App() {
   const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    call("/todo", "GET", null).then(response => setItems(response.data));
+    call("/todo", "GET", null).then(response => {
+      setItems(response.data);
+      setLoading(true);
+    });
   }, []);
 
   const addItem = item => {
     call("/todo", "POST", item).then(response => setItems(response.data));
   };
 
-  const deleteItem = item => {
-    call("/todo", "DELETE", item).then(response => setItems(response.data));
-  };
-
   const editItem = item => {
     call("/todo", "PUT", item).then(response => setItems(response.data));
+  };
+
+  const deleteItem = item => {
+    call("/todo", "DELETE", item).then(response => setItems(response.data));
   };
 
   let todoItems = items.length > 0 && (
@@ -66,8 +70,9 @@ function App() {
     </AppBar>
   );
 
-  return (
-    <div className='App'>
+  /* 로딩중이 아닐 때 렌더링 할 부분 */
+  let todoListPage = (
+    <div>
       {navigationBar} {/* 네비게이션 바 렌더링 */}
       <Container maxWidth='md'>
         <AddTodo addItem={addItem} />
@@ -75,6 +80,18 @@ function App() {
       </Container>
     </div>
   );
+
+  /* 로딩중일 때 렌더링 할 부분 */
+  let loadingPage = <h1> 로딩중.. </h1>;
+  let content = loadingPage;
+
+  if (!loading) {
+    /* 로딩중이 아니면 todoListPage를 선택*/
+    content = todoListPage;
+  }
+
+  /* 선택한 content 렌더링 */
+  return <div className='App'>{content}</div>;
 }
 
 export default App;
